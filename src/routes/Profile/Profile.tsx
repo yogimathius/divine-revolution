913fb27dc6317e85adca808a86d3afeabe616cb5
Mathius <info@yogimathius.dev>
2023-05-12 15:31:26 -0700

    chore: add styles to Profile

--- src/routes/Profile/Profile.tsx
+++ src/routes/Profile/Profile.tsx
@@ -1,9 +1,24 @@
-const Profile = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+import { useContext } from 'react';
+import { AuthContext } from '../../context';
 
-export default Profile
+const ProfilePage = () => {
+  const { user } = useContext(AuthContext);
+
+  return (
+    <div className="container mx-auto p-4">
+      <div className="bg-white p-4 shadow-md rounded-md">
+        <p className="mb-2">
+          <span className="font-semibold">Username:</span> {user.username}
+        </p>
+        <p className="mb-2">
+          <span className="font-semibold">Email:</span> {user.email}
+        </p>
+        <p className="mb-2">
+          <span className="font-semibold">Bio:</span> {user.bio}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ProfilePage;
