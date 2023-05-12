eda57884b2c34092bb9309dc63ed64614604b419
Mathius <info@yogimathius.dev>
2023-05-12 13:22:03 -0700

    chore: handle basic login feature

--- src/graphql/mutations/login.ts
+++ src/graphql/mutations/login.ts
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client';
 
 export const loginMutation = gql`
-  mutation login($username: string, $password: string) {
+  mutation login($username: String!, $password: String!) {
     login(input: {username: $username, password: $password}) {
       expiration
       token
--- src/routes/Login/Login.tsx
+++ src/routes/Login/Login.tsx
@@ -1,23 +1,78 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { TextField, Button } from '@mui/material';
-
-
+import { useMutation } from '@apollo/client';
+import { loginMutation } from '../../graphql/mutations/login';
+import jwt from 'jsonwebtoken';
 
 const Login= () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [token, setToken] = useState<string | null>(null);
+
+  // Function to set the token in localStorage
+  const setAuthToken = (token: string) => {
+    localStorage.setItem('authToken', token);
+    setToken(token);
+  };
+
+  // Function to remove the token from localStorage
+  const removeAuthToken = () => {
+    localStorage.removeItem('authToken');
+    setToken(null);
+  };
+
+  useEffect(() => {
+    // Check if a token exists in localStorage on initial render
+    const storedToken = localStorage.getItem('authToken');
+    if (storedToken) {
+      setToken(storedToken);
+    }
+  }, []);
+
+  // TODO fix bug in this function about util.inherits not being a function
+  // // Check if the token has expired
+  // const isTokenExpired = () => {
+  //   if (!token) {
+  //     return true; // Token not found, consider it expired
+  //   }
+  //   const decodedToken = jwt.decode(token); // You can use a JWT library to decode the token
+  //   console.log(decodedToken);
+    
+  //   if (!decodedToken) {
+  //     return false
+  //   }
+  //   const expirationDate = new Date(decodedToken * 1000);
+  //   const currentTime = new Date();
+  //   return expirationDate < currentTime;
+  // };
+
+  // isTokenExpired()
+  const [login, {error, data}] = useMutation(loginMutation, {
+    variables: {
+      username,
+      password
+    }
+  })
 
   const handleLogin = () => {
     // Reset form fields
     console.log(username, password);
-    
-    setUsername('');
-    setPassword('');
+    login()
   };
-
   
+  useEffect(() => {
+    if (data) {
+      console.log(data);
+      
+      setAuthToken(data.login.token)
+      setUsername('');
+      setPassword('');
+    }
+  }, [data])
+
   return (
   <div className="flex flex-col items-center justify-center space-y-4 bg-blue-100 rounded-lg mx-auto p-24 h-max">
+      {error ? <p>Oh no! {error.message}</p> : null}
       <h2 className="text-2xl font-semibold mb-2">Login</h2>
       <TextField
         label="Username"
