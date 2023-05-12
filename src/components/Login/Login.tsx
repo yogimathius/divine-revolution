18ae9490af0713bef0b6a23c16b870cadf545b0d
Mathius <info@yogimathius.dev>
2023-05-12 12:29:05 -0700

    chore: use mui components in Login

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,39 +1,41 @@
-import React, { useState } from 'react';
-import { TextField, Button,  AlphaCard, Text } from '@shopify/polaris';
+import { useState } from 'react';
+import { TextField, Button } from '@mui/material';
 
-interface LoginProps {
-  onLogin: (username: string, password: string) => void;
-}
 
-const Login: React.FC<LoginProps> = ({ onLogin }) => {
+
+const Login= () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleLogin = (e: React.FormEvent) => {
-    e.preventDefault();
-    onLogin(username, password);
+  const handleLogin = () => {
     // Reset form fields
+    console.log(username, password);
+    
     setUsername('');
     setPassword('');
   };
 
+  
   return (
   <div className="flex flex-col items-center justify-center space-y-4 bg-blue-100 rounded-lg mx-auto p-24 h-max">
-      <h2 className="text-2xl font-semibold mb-6">Login</h2>
+      <h2 className="text-2xl font-semibold mb-2">Login</h2>
       <TextField
         label="Username"
         value={username}
-        onChange={setUsername}
+        onChange={(e) => setUsername(e.target.value)}
         autoComplete="username"
+        variant="outlined"
+        sx={{bgcolor: 'background.paper'}}
       />
       <TextField
         label="Password"
         value={password}
-        onChange={setPassword}
+        onChange={(e) => setPassword(e.target.value)}
         autoComplete="current-password"
         type="password"
+        sx={{bgcolor: 'background.paper'}}
       />
-      <Button submit>Login</Button>
+      <Button variant='contained' onClick={() => handleLogin()}>Login</Button>
     </div>
   );
 };
