a3c8435e5cb542ce260e66c1c52f54a7d745b522
Mathius <info@yogimathius.dev>
2023-05-12 14:19:14 -0700

    chore: rename root to loaders

--- src/routes/loaders.tsx
+++ src/routes/loaders.tsx
@@ -2,7 +2,8 @@ import { redirect } from "react-router-dom";
 
 const loader = async () => {
   const storedToken = localStorage.getItem('authToken');
-
+  console.log('token in loader: ', !storedToken);
+  
   if (!storedToken) {
     return redirect("/login");
   }
