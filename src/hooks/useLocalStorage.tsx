55b34bf15c1a798b4898cb97e4bc45ebde4e6307
Mathius <info@yogimathius.dev>
2023-05-12 22:34:51 -0700

    feat: add localStorage hook

---
+++ src/hooks/useLocalStorage.tsx
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react';
+
+const useLocalStorage = (key: string, initialValue?: string | number) => {
+  const [value, setValue] = useState(() => {
+    const storedValue = localStorage.getItem(key);
+    console.log(!storedValue, key);
+    if (!storedValue) {
+      return initialValue
+    } else {
+      return JSON.parse(storedValue)
+    }
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
+};
+
+export default useLocalStorage;
