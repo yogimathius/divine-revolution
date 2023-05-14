5628d2ca189e9b27fff5279ee882528228aa329f
Mathius <info@yogimathius.dev>
2023-05-13 21:35:25 -0700

    chore: use Show component in Profile

--- src/routes/Profile/Profile.tsx
+++ src/routes/Profile/Profile.tsx
@@ -2,6 +2,7 @@ import { useContext, useEffect } from 'react';
 import { AuthContext } from '../../context';
 import useGetUserQuery from '../../graphql/hooks/useGetUserQuery';
 import { useLocalStorage } from '../../hooks';
+import { Show } from '../../components/Profile';
 
 const ProfilePage = () => {
   const { user, setUser } = useContext(AuthContext);
@@ -31,24 +32,8 @@ const ProfilePage = () => {
   
   return (
     <div className='h-screen mt-8 max-w-4xl w-full mx-4 sm:mx-auto '>
-      <div className=" border-b border-gray-300 p-6  ">
-        <h2 className="text-2xl font-bold mb-4">Profile</h2>
-        <div className="pb-4 mb-4">
-          <p className="text-lg font-semibold">
-            <span className="text-blue-600">Username:</span> {user.username}
-          </p>
-          <p className="text-lg font-semibold">
-            <span className="text-blue-600">Email:</span> {user.email}
-          </p>
-        </div>
-        <div>
-          <p className="text-lg font-semibold">
-            <span className="text-blue-600">Bio:</span> {user.bio}
-          </p>
-        </div>
-      </div>
+      <Show user={user} />
     </div>
-
   );
 };
 
